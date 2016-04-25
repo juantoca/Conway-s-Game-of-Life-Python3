21a1f0af45574bf12ee285d380162da31d6cdedf
BuildTools <elan17.programacion@gmail.com>
2016-04-25 15:39:50 +0200

    Prueba

--- Nucleo.py
+++ Nucleo.py
@@ -1,3 +1,17 @@
-# UNDER COMPLETE REWRITING
 
-print("Hello")
+
+class Mundo:  # Clase que maneja el juego
+
+    def __init__(self, center=(0, 0), coordinates=(), interfaz=None,
+                 tiempo=0, limite=-1, print_during=False, debugging=False):
+        # Self-Explanatory
+        self.variacion = ((-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0))
+        self.debugging = debugging
+        self.interfaz = interfaz
+        self.center = center
+        self.toBorn = []
+        self.toKill = []
+        self.limite = limite
+        self.tiempo = tiempo
+        self.print_during = print_during
+        self.builder(self.coordinates)
