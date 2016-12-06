8d050c076f0f905ab37819d60f83af6397581741
elan17 <elan17.programacion@gmail.com>
2016-12-06 19:47:15 +0100

    Erased unnecessary atribute at Nucleo: center

--- Nucleo.py
+++ Nucleo.py
@@ -8,13 +8,12 @@ import multiprocessing
 
 class Mundo:  # Clase que maneja el juego
 
-    def __init__(self, center=(0, 0), coordinates=(), interfaz=None,
+    def __init__(self, coordinates=(), interfaz=None,
                  tiempo=0, limite=-1, print_during=False, debugging=False):
         # Self-Explanatory
         self.variacion = ((-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0))
         self.debugging = debugging
         self.interfaz = interfaz
-        self.center = center
         self.coordinates = coordinates
         self.cells = {}
         self.limite = limite
